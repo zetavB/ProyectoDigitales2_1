991a23c56ba9c7e8a874d87c69fb95abd6810bac
valeviales <73159764+valeviales@users.noreply.github.com>
2021-10-05 01:52:01 -0600

    Update genClock.v
    Agregando generador de relojes

--- genClock.v
+++ genClock.v
@@ -1,4 +1,24 @@
-module genClock(
-		
+`timescale 1ns/1ps
+
+module genClock(input clk,
+            input reset,
+            output reg clk_ff,
+            output reg clk_2ff,
+            output reg clk_3ff);
+
+  always @ (posedge clk) begin
+    if (reset==1) begin        
+      clk_ff <= 1'h0; //reseteando clk
+      clk_2ff <= 1'h0; //reseteando clk
+      clk_3ff <= 1'h0; //reseteando clk           
+    end 
+    else begin         //cambia de alto a bajo o de bajo a alto                             
+          clk_3ff <= ~clk_3ff;               
+          if (~clk_3ff) 
+            clk_2ff <= ~clk_2ff;
+          if (~clk_2ff & ~clk_3ff) 
+            clk_ff <= ~clk_ff;
+    end
+  end		
 		);
 endmodule // genClock
