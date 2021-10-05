2baa1ca4fe92de8484d72c813d06bb1a93730d23
zetavB <jordi.louzao@gmail.com>
2021-10-05 15:39:26 -0700

    Se arregla error en probador

--- 8_32/testbench.v
+++ 8_32/testbench.v
@@ -2,7 +2,6 @@
 
 `include "conv8_32.v"
 `include "tester.v"
-`include "genClock.v"
 
 module testbench;
 	wire reset, clk, clk_f, clk_2f, clk_4f, in8;
@@ -12,24 +11,15 @@ module testbench;
 
 	tester test1(/*AUTOINST*/
 		     // Outputs
+		     .in_data8		(in_data8[7:0]),
+		     .reset		(reset),
+		     .in8		(in8),
 		     .clk		(clk),
 		     .clk_4f		(clk_4f),
 		     .clk_f		(clk_f),
-		     .out_data32	(out_data32[31:0]),
-		     .reset		(reset),
-		     .in8		(in8),
 		     // Inputs
-		     .in_data8		(in_data8[7:0]),
-		     .out32		(out32));
-
-	genClock clk_gen (/*AUTOINST*/
-			  // Outputs
-			  .clk_f		(clk_f),
-			  .clk_2f		(clk_2f),
-			  .clk_4f		(clk_4f),
-			  // Inputs
-			  .clk			(clk),
-			  .reset		(reset));
+		     .out32		(out32),
+		     .out_data32	(out_data32[31:0]));
 
 	conv8_32 testconv (/*AUTOINST*/
 			   // Outputs
