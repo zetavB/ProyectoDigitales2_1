9ceabe042b10212768e16b7741d860cbe001cfcb
zetavB <jordi.louzao@gmail.com>
2021-10-05 18:45:13 -0700

    Se depura el striping y se deja funcional

--- Stripe-Unstripe/probadorStripes.v
+++ Stripe-Unstripe/probadorStripes.v
@@ -22,7 +22,10 @@ module probadorStripes(
 	   $dumpfile("stripingTest");	// Nombre de archivo del "dump"
 	   $dumpvars;			// Directiva para "dumpear" variables
 	   {reset} = 'b0; 	// A todos los bits, asigna un tipo bit con valor 0.
-	   
+	   repeat (5) begin
+	      @(posedge clk_2f);
+	   end
+	   validIn <= 1;
 	   repeat (1600) begin		//
               @(posedge clk_2f);	// Espera/sincroniza con el flanco positivo del reloj
 	      dataIn <= dataIn - 1;
@@ -49,12 +52,11 @@ module probadorStripes(
    
    
    always #100  clk_2f 	<= ~clk_2f;
-   always #50 validIn <= 1;
    always #50 valid0 <= 1;
    always #50 valid1 <= 1;
    always #50 uvalid0 <= 1;
    always #50 uvalid1 <= 1;
-   always #150 reset <= 0;
+   always #200 reset <= 0;
    always #150 ureset <= 0;
    
    
